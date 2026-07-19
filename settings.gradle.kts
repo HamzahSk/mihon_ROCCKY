f664ad5fbe00ccb7fd8590b5e73804e976947f9f
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-19 15:20:14 +0700

    Last Sync: 2026-07-19 15:20 (Mobile)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "Mihon"
+rootProject.name = "Rikomik"
 include(":app")
 include(":baseline-profile")
 include(":core-metadata")
