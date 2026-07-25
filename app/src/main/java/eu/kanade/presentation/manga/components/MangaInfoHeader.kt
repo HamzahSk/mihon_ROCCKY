cb3ae89184812912c7fc6f034e634ead8b5d7440
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-25 21:10:42 +0700

    Last Sync: 2026-07-25 21:10 (Mobile)

--- app/src/main/java/eu/kanade/presentation/manga/components/MangaInfoHeader.kt
+++ app/src/main/java/eu/kanade/presentation/manga/components/MangaInfoHeader.kt
@@ -63,8 +63,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
